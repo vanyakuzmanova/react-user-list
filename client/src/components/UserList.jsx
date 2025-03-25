6ea211e8d0a1fd636e623e6e912962d711bd46c7
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-03-26 01:02:42 +0200

    Show user info modal

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
+
 import userService from "../services/userService";
 
-import Search from "./Search";
 import Pagination from "./Pagination";
+import Search from "./Search";
 import UserListItem from "./UserListItem";
 import UserCreate from "./UserCreate";
+import UserInfo from "./UserInfo";
 
 export default function UserList() {
     const [users, setUsers] = useState([]);
     const [showCreate, setShowCreate] = useState(false);
+    const [userIdInfo, setUserIdInfo] = useState();
 
     useEffect(() => {
         userService.getAll()
             .then(result => {
-                console.log(result);
                 setUsers(result);
             })
     }, []);
@@ -22,115 +24,100 @@ export default function UserList() {
         setShowCreate(true);
     };
 
-    const closeCreateUserHandler = () => {
+    const closeCreateUserClickHandler = () => {
         setShowCreate(false);
     };
 
-    const saveCreateUserHandler = async (e) => {
-        //Stop default form refresh behavior
+    const saveCreateUserClickHandler = async (e) => {
+        // Stop default refresh behaviour
         e.preventDefault();
 
-        //Get form data
+        // Get form date
         const formData = new FormData(e.target);
         const userData = Object.fromEntries(formData);
 
-        //create new user to server
+        // create new user on server
         const newUser = await userService.create(userData);
-        console.log(newUser)
 
-        //update local state
-        setUsers((state) => [...state, newUser])
+        // update local state
+        setUsers(state => [...state, newUser])
 
-        //close modal
+        // close modal 
         setShowCreate(false);
-      
-    }
+    };
 
-
+    const userInfoClickHandler = (userId) => {
+        setUserIdInfo(userId);
+    };
 
     return (
         <section className="card users-container">
             <Search />
 
             {showCreate && (
-                <UserCreate 
-                    onClose={closeCreateUserHandler}
-                    onSave={saveCreateUserHandler}  
+                <UserCreate
+                    onClose={closeCreateUserClickHandler}
+                    onSave={saveCreateUserClickHandler}
                 />)
-        }
+            }
 
-            {/* <!-- Table component --> */}
+            {userIdInfo && (
+                <UserInfo
+                    userId={userIdInfo}
+                />
+            )}
+
             <div className="table-wrapper">
-                <div>
+                <div className="overlays">
+
                     {/* <!-- Overlap components  --> */}
 
-                    {/* <!-- <div className ="loading-shade"> --> */}
+                    {/* <!-- <div className="loading-shade"> --> */}
                     {/* <!-- Loading spinner  --> */}
-                    {/* <!-- <div className ="spinner"></div> --> */}
+                    {/* <!-- <div className="spinner"></div> --> */}
                     {/* <!--  */}
                     {/* No users added yet  --> */}
 
-                    {/* <div className ="table-overlap">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="triangle-exclamation"
-                className ="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
-                ></path>
-              </svg>
-              <h2>There is no users yet.</h2>
-            </div> */}
-
-                    {/* <!-- No content overlap component  --> */}
-
-                    {/* <div className ="table-overlap"> 
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="triangle-exclamation"
-                className ="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
-                ></path>
-              </svg>
-              <h2>Sorry, we couldn't find what you're looking for.</h2>
-            </div>  */}
+                    {/* <div className="table-overlap">
+                    <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="triangle-exclamation"
+                        className="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
+                        ></path>
+                    </svg>
+                    <h2>There is no users yet.</h2>
+                </div> */}
 
                     {/* <!-- On error overlap component  --> */}
 
-                    {/* <div className ="table-overlap">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="triangle-exclamation"
-                className ="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
-                ></path>
-              </svg>
-              <h2>Failed to fetch</h2>
-            </div> 
-           </div>  */}
+                    {/* <div className="table-overlap">
+                    <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="triangle-exclamation"
+                        className="svg-inline--fa fa-triangle-exclamation Table_icon__+HHgn"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
+                        ></path>
+                    </svg>
+                    <h2>Failed to fetch</h2>
+                </div> */}
+                    {/* <!-- </div> --> */}
                 </div>
 
                 <table className="table">
@@ -191,17 +178,16 @@ export default function UserList() {
                     <tbody>
                         {users.map(user => <UserListItem
                             key={user._id}
+                            onInfoClick={userInfoClickHandler}
                             {...user}
                         />)}
-
                     </tbody>
                 </table>
             </div>
 
-            {/* New user button  */}
             <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
 
             <Pagination />
         </section>
-    )
+    );
 }
--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,12 +4,18 @@ export default {
     async getAll() {
         const response = await fetch(baseUrl);
         const result = await response.json();
-        const users = Object.values(result);
+        const users = Object.values(result);    
 
         return users;
     },
+    async getOne(userId) {
+        const response = await fetch(`${baseUrl}/${userId}`);
+        const user = await response.json();
+
+        return user;
+    },
     async create(userData) {
-        const { country, city, street, streetNumber, postData } = userData;
+        const { country, city, street, streetNumber, ...postData } = userData;
 
         postData.address = { country, city, street, streetNumber };
         postData.createdAt = new Date().toISOString();
@@ -21,8 +27,7 @@ export default {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(userData)
-
-        });
+        })
 
         const result = await response.json();
         return result;
