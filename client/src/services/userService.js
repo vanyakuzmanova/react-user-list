997f631bd9a408843a1bdea0aac49329ef6129db
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-03-25 17:03:06 +0200

    fix post Data

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,21 +1,27 @@
 const baseUrl = 'http://localhost:3030/jsonstore/users';
 
-export default{
-    async getAll(){
+export default {
+    async getAll() {
         const response = await fetch(baseUrl);
         const result = await response.json();
-        const users =  Object.values(result);
-       
+        const users = Object.values(result);
+
         return users;
     },
-    async create(userData){
-        const response = await fetch(baseUrl,{
+    async create(userData) {
+        const { country, city, street, streetNumber, postData } = userData;
+
+        postData.address = { country, city, street, streetNumber };
+        postData.createdAt = new Date().toISOString();
+        postData.updatedAt = new Date().toISOString();
+
+        const response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(userData)
-           
+
         });
 
         const result = await response.json();
